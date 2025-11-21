ada69d89f36348afc20a7f7198116d20901c5454
sonvuuu28 <Vuthienson280104@gmail.com>
2025-11-21 08:50:10 +0700

    modify main

--- etl/Main.py
+++ etl/Main.py
@@ -1,33 +1,35 @@
+import os
+from uuid import UUID
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import StringType
+from cassandra.util import datetime_from_uuid1
+
 from Cassandra import Cassandra
 from MySql import MySql
-import os
-from pyspark.sql.functions import *
-from uuid import UUID
-from cassandra.util import *
 
-mysql_driver = os.path.abspath("../driver/mysql-connector-j-8.0.33.jar")
+# ===========================================================
+# SPARK CONFIG
+# ===========================================================
 
+MYSQL_JAR = os.path.abspath("../driver/mysql-connector-j-8.0.33.jar")
 
 spark = (
     SparkSession.builder.config(
         "spark.jars.packages", "com.datastax.spark:spark-cassandra-connector_2.12:3.1.0"
     )
-    .config("spark.driver.extraClassPath", mysql_driver)
-    .config("spark.executor.extraClassPath", mysql_driver)
+    .config("spark.driver.extraClassPath", MYSQL_JAR)
+    .config("spark.executor.extraClassPath", MYSQL_JAR)
     .getOrCreate()
 )
 
-# Declare db
+# DB Instances
 cass = Cassandra(spark)
 mysql = MySql(spark)
 
-
-def spark_read_file(path):
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    csv = os.path.join(base_dir, path)
-    df = spark.read.csv(csv, header=True)
-    return df
+# ===========================================================
+#      UDF
+# ===========================================================
 
 
 @udf(returnType=StringType())
@@ -40,74 +42,79 @@ def extract_timestamp_from_uuid(uuid_str):
         return None
 
 
-def pre_processing_data(df):
-    # Create timestamp based on uuid
-    df = df.withColumn("system_ts", extract_timestamp_from_uuid(col("create_time")))
-
-    # Select useful columns
-    df = df.select(
-        "create_time",
-        "system_ts",
-        "job_id",
-        "custom_track",
-        "bid",
-        "campaign_id",
-        "group_id",
-        "publisher_id",
-    )
-
-    # Convert system_ts to timestamp
-    df = df.withColumn(
-        "system_ts", to_timestamp(col("system_ts"), "yyyy-MM-dd HH:mm:ss")
-    )
-
-    # Select useful rows
-    df = df.filter(col("job_id").isNotNull())
-    df = df.filter(col("custom_track").isNotNull())
-
-    return df
+# ===========================================================
+#      UTILITY READER
+# ===========================================================
 
 
-def process_data(df):
-    # Get neccessary event types
+def spark_read_file(path):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    return spark.read.csv(os.path.join(base_dir, path), header=True)
+
+
+# ===========================================================
+#              DATA TRANSFORM PIPELINE
+# ===========================================================
+
+
+class DataTransformer:
     valid_events = ["click", "conversion", "qualified", "unqualified"]
-    df = df.filter(col("custom_track").isin(valid_events))
 
-    # Split data and hour
-    df = df.withColumn("dates", to_date(col("system_ts"))).withColumn(
-        "hours", hour(col("system_ts"))
-    )
-
-    # Using pivot to advoid multiple joins
-    result_df = (
-        df.groupBy(
-            "job_id", "dates", "hours", "publisher_id", "campaign_id", "group_id"
+    @staticmethod
+    def preprocess(df):
+        # Get timestamp from uuid
+        df = df.withColumn("system_ts", extract_timestamp_from_uuid(col("create_time")))
+        df = df.withColumn(
+            "system_ts", to_timestamp("system_ts", "yyyy-MM-dd HH:mm:ss")
         )
-        .pivot("custom_track", valid_events)
-        .agg(
-            count("*").alias("count"),
+
+        # Select useful rows and cols
+        df = df.select(
+            "create_time",
+            "system_ts",
+            "job_id",
+            "custom_track",
+            "bid",
+            "campaign_id",
+            "group_id",
+            "publisher_id",
+        ).filter("job_id IS NOT NULL AND custom_track IS NOT NULL")
+
+        return df
+
+    @staticmethod
+    def aggregate(df):
+        df = df.filter(col("custom_track").isin(DataTransformer.valid_events))
+
+        # Split dates and hours from timestamp
+        df = df.withColumn("dates", to_date("system_ts"))
+        df = df.withColumn("hours", hour("system_ts"))
+
+        pivot_df = (
+            df.groupBy(
+                "job_id", "dates", "hours", "publisher_id", "campaign_id", "group_id"
+            )
+            .pivot("custom_track", DataTransformer.valid_events)
+            .agg(count("*").alias("count"))
         )
-    )
 
-    # Rename columns
-    for event in valid_events:
-        result_df = result_df.withColumnRenamed(f"{event}_count", event)
+        # Rename {event}_count → event
+        for e in DataTransformer.valid_events:
+            pivot_df = pivot_df.withColumnRenamed(f"{e}_count", e)
 
-    # Calculate metrics
-    agg_df = df.groupBy("job_id", "publisher_id", "campaign_id", "group_id").agg(
-        round(sum("bid"), 2).alias("spend_hour"), round(avg("bid"), 2).alias("bid_set")
-    )
+        # Spend & bid
+        metric_df = df.groupBy("job_id", "publisher_id", "campaign_id", "group_id").agg(
+            round(sum("bid"), 2).alias("spend_hour"),
+            round(avg("bid"), 2).alias("bid_set"),
+        )
 
-    result_df = result_df.join(
-        agg_df, on=["job_id", "publisher_id", "campaign_id", "group_id"], how="left"
-    )
+        return pivot_df.join(
+            metric_df, ["job_id", "publisher_id", "campaign_id", "group_id"], "left"
+        )
 
-    return result_df
-
-
-def replace_null(df):
-    return df.fillna(
-        {
+    @staticmethod
+    def fill_null(df):
+        fill_values = {
             "click": 0,
             "conversion": 0,
             "qualified": 0,
@@ -115,107 +122,97 @@ def replace_null(df):
             "spend_hour": 0,
             "bid_set": 0,
         }
-    )
+        return df.fillna(fill_values)
+
+    @staticmethod
+    def post_process(df):
+        # Add Primary Key
+        df = df.withColumn("id", monotonically_increasing_id())
+
+        # Mark update dates
+        df = df.withColumn("updated_at", current_timestamp())
+        return df.select(
+            "id",
+            "job_id",
+            "dates",
+            "hours",
+            "company_id",
+            "group_id",
+            "campaign_id",
+            "publisher_id",
+            "click",
+            "conversion",
+            "qualified",
+            "unqualified",
+            "bid_set",
+            "spend_hour",
+            "sources",
+            "updated_at",
+        )
+
+    @staticmethod
+    def transform_full(df):
+        df = DataTransformer.preprocess(df)
+        df = DataTransformer.aggregate(df)
+        df = df.withColumn("sources", lit("Cassandra"))
+        df = DataTransformer.fill_null(df)
+
+        job_df = mysql.read("job").select(col("id").alias("job_id"), "company_id")
+        df = df.join(job_df, "job_id", "left")
+
+        return DataTransformer.post_process(df)
 
 
-def post_process(df):
-    # Add Primary key
-    df = df.withColumn("id", monotonically_increasing_id())
-
-    # Mark time for update
-    df = df.withColumn("updated_at", current_timestamp())
-
-    df = df.select(
-        "id",
-        "job_id",
-        "dates",
-        "hours",
-        "company_id",
-        "group_id",
-        "campaign_id",
-        "publisher_id",
-        "click",
-        "conversion",
-        "qualified",
-        "unqualified",
-        "bid_set",
-        "spend_hour",
-        "sources",
-    )
-    return df
+# ===========================================================
+#              SYNC CHECK BETWEEN MYSQL & CASS
+# ===========================================================
 
 
-def my_transform(df):
-    # Prepocessing
-    processed_df = pre_processing_data(df)
+class DataSync:
 
-    # Calculate metrics
-    final_df = process_data(processed_df)
+    @staticmethod
+    def last_mysql_date():
+        df = mysql.read("campaign")
+        return df.select(max("updated_at")).first()[0]
 
-    # Final output
-    final_df = final_df.withColumn("sources", lit("Cassandra"))
-
-    # Fill null
-    final_df = replace_null(final_df)
-
-    # Join job to get company_id
-    job_df = mysql.read("job")
-    job_df = job_df.select(col("id").alias("job_id"), "company_id")
-    final_df = final_df.join(job_df, on="job_id", how="left")
-
-    # Post process
-    final_df = post_process(final_df)
-
-    return final_df
+    @staticmethod
+    def last_cassandra_date():
+        df = cass.read("tracking")
+        df = df.withColumn("create_time", extract_timestamp_from_uuid("create_time"))
+        df = df.withColumn("create_time", to_timestamp("create_time"))
+        df = df.withColumn(
+            "create_time", from_utc_timestamp("create_time", "Asia/Ho_Chi_Minh")
+        )
+        return df.select(max("create_time")).first()[0]
 
 
-def date_from_mysql():
-    df = mysql.read("campaign")
-    a = df.select(max("updated_at").alias("max")).collect()[0]
-    return a["max"]
+# ===========================================================
+#                       MAIN ETL
+# ===========================================================
 
 
-def date_from_cassandra():
+def run_etl():
     df = cass.read("tracking")
-    df = df.withColumn("create_time", extract_timestamp_from_uuid(col("create_time")))
-    df = df.withColumn(
-        "create_time", to_timestamp("create_time", "yyyy-MM-dd HH:mm:ss")
-    )
-    df = df.withColumn(
-        "create_time",
-        from_utc_timestamp("create_time", "Asia/Ho_Chi_Minh"),  # đổi sang giờ VN
-    )
-    a = df.select(max("create_time").alias("max")).collect()[0]
-    return a["max"]
-
-
-if __name__ == "__main__":
-    # Insert data to data lake
-    print("insert Cassandra")
-    tracking_df = spark_read_file("../data/cassandra/tracking.csv")
-    cass.insert("tracking", tracking_df)
-
-    # Insert data to data warehouse
-    print("insert MySQL")
-    job_df = spark_read_file("../data/mysql/job.csv")
-    mysql.insert("job", job_df)
-
-    # read data from Cassandra
-    df = cass.read("tracking")
-
-    # Transfrom data
-    df = my_transform(df)
-
-    # Insert data to data warehouse
+    df = DataTransformer.transform_full(df)
     mysql.insert("campaign", df)
 
+
+# ===========================================================
+#                       ENTRY POINT
+# ===========================================================
+
+if __name__ == "__main__":
+    # Initial load
+    print("Insert Cassandra")
+    cass.insert("tracking", spark_read_file("../data/cassandra/tracking.csv"))
+
+    print("Insert MySQL")
+    mysql.insert("job", spark_read_file("../data/mysql/job.csv"))
+
+    # First ETL
+    run_etl()
+
+    # Continuous sync
     while True:
-        if date_from_mysql() < date_from_cassandra():
-            # read data from Cassandra
-            df = cass.read("tracking")
-
-            # Transfrom data
-            df = my_transform(df)
-
-            # Insert data to data warehouse
-            mysql.insert("campaign", df)
+        if DataSync.last_mysql_date() < DataSync.last_cassandra_date():
+            run_etl()
